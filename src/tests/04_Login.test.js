61822a20649c5ee072eda76cddc38aeac640cf65
Harumi Maeda <harumiumaeda@gmail.com>
2022-08-05 22:28:43 -0300

    novos ajustes req-4

--- src/tests/04_Login.test.js
+++ src/tests/04_Login.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
-import { screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Login from '../pages/Login';
+
+import { MemoryRouter } from 'react-router-dom';
 
 describe('1. Desenvolva testes para atingir 90% de cobertura da tela de Login', () => {
   it('Testa se o input de nome e e-mail são renderizados', () => {
@@ -40,5 +42,32 @@ describe('1. Desenvolva testes para atingir 90% de cobertura da tela de Login', 
 
     const playButton = screen.getByRole('button', { name: 'Play' });
     expect(playButton).toBeDisabled();
+  });
+
+  it('Testa se o botão "Play" direciona para a rota /game', async () => {
+    const { history } = renderWithRouterAndRedux(<Login />);
+
+    const inputName = screen.getByPlaceholderText(/Digite seu nome/i);
+    userEvent.type(inputName, 'Samanta');
+    expect(inputName).toHaveValue('Samanta');
+
+    const inputEmail = screen.getByPlaceholderText(/Digite seu E-mail/i);
+    userEvent.type(inputEmail, 'samanta@trybe.com');
+    expect(inputEmail).toHaveValue('samanta@trybe.com');
+
+    const playButton = screen.getByRole('button', { name: 'Play' });
+    userEvent.click(playButton);
+
+    await waitFor(() => { expect(history.location.pathname).toBe('/game')});
+    
+  });
+
+  it('Testa se o botão "Settings" direciona para a rota /settings', async () => {
+    const { history } = renderWithRouterAndRedux(<Login />);
+
+    const settingsButton = screen.getByRole('button', { name: /settings/i })
+    userEvent.click(settingsButton);
+
+    await waitFor(() => { expect(history.location.pathname).toBe('/settings')});
   });
 })
