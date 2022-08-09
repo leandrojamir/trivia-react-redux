2e1a2acc05026cd615fb4404c367e84f339cfdf2
marcos <njsu5@hotmail.com>
2022-08-09 17:57:08 -0300

    grupo-7-Requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,33 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { logout } from '../redux/actions';
 
-function Ranking() {
+function Ranking({ logoutLigin }) {
+  const history = useHistory();
   return (
     <div>
       <h1 data-testid="ranking-title">Ranking</h1>
+      <button
+        type="button"
+        data-testid="btn-go-home"
+        onClick={ () => {
+          logoutLigin();
+          history.push('/');
+        } }
+      >
+        Play Again
+      </button>
     </div>
   );
 }
 
-export default Ranking;
+Ranking.propTypes = {
+  logoutLigin: PropTypes.func,
+}.isRequired;
+
+const mapDispatchToProps = (dispatch) => ({
+  logoutLigin: () => dispatch(logout()),
+});
+export default connect(null, mapDispatchToProps)(Ranking);
