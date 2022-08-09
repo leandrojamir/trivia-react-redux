3942c1c3123cdaa5c7c22f6f82171f9af5bcad52
Marcos Souza Brito <82009172+marcos2872@users.noreply.github.com>
2022-08-09 20:07:58 -0300

    Revert "[ Grupo 7 ] Project Trivia"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
-import Settings from './pages/Settings';
-import Game from './pages/Game';
-import Feedback from './pages/Feedback';
-import Ranking from './pages/Ranking';
+import logo from './trivia.png';
+import './App.css';
 
 export default function App() {
   return (
-    <div>
-      <Link to="/">Login</Link>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/settings" component={ Settings } />
-        <Route exact path="/game" component={ Game } />
-        <Route exact path="/feedback" component={ Feedback } />
-        <Route exact path="/ranking" component={ Ranking } />
-      </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>SUA VEZ</p>
+      </header>
     </div>
   );
 }
--- src/pages/Settings.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Settings() {
-  return (
-    <div data-testid="settings-title">Title</div>
-  );
-}
-
-export default Settings;
